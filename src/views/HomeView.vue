a7e3b23d36987109292ba5e9e46265b041898f85
John Christian B. Narbaja <narbajajc@gmail.com>
2024-09-09 23:35:10 +0800

    Fetched API in OpenWeather

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,11 +10,21 @@
         <ul class="absolute bg-weather-secondary w-full text-white shadow-md py-2 px-1"
         v-if="mapboxSearchResults"
         >
-          <li v-for="searchResult in mapboxSearchResults"
-          :key="searchResult.id"
-          class="py-2 cursor-pointer">
-          {{searchResult.place_name}}
-          </li>
+          <p v-if="searchError">Sorry, Something went wrong</p>
+
+          <p v-if="!serverError && mapboxSearchResults.length === 0">No results try different term</p>
+
+          <template v-else> 
+            <li v-for="searchResult in mapboxSearchResults"
+            :key="searchResult.id"
+            class="py-2 cursor-pointer"
+            @click="previewCity(searchResult)"
+            >
+            
+
+            {{searchResult.place_name}}
+            </li>
+          </template>
         </ul>
     </div>
 
@@ -24,22 +34,45 @@
 <script setup>
 import { ref } from "vue";
 import axios from "axios";
+import {useRouter} from "vue-router";
 
+const router = useRouter();
+const previewCity= (searchResult) => {
+  console.log(searchResult);
+  const [city, state] = searchResult.place_name.split(",");
+  router.push({
+    name: 'cityView',
+    params: {state: state.replaceAll(" ",""), city: city},
+    query:{
+      lat:searchResult.geometry.coordinates[1],
+      lng:searchResult.geometry.coordinates[0],
+      preview: true,
+    }
+  })
+};
+
+//MapBox API
 const mapboxAPIKey ="pk.eyJ1Ijoiam9obm5vdGUiLCJhIjoiY20wbmsybGJrMGVxNzJscHZxamZ4cWU3diJ9.6qq6aC3O8WswGkN3NY6nMg"
 const searchQuery = ref("");
 const queryTimeout = ref(null);
 const mapboxSearchResults = ref(null);
+const searchError = ref(null);
 
 const getSearchResults = () => {
   clearTimeout(queryTimeout.value);
   queryTimeout.value = setTimeout(async() => {
     if (searchQuery.value  !== "") {
-      const result = await axios.get(
+      try {
+        const result = await axios.get(
         // `https://api.mapbox.com/search/geocode/v6/forward?q=${searchQuery.value}.json?access_token=${mapboxAPIKey}`
         `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(searchQuery.value)}.json?access_token=${mapboxAPIKey}&types=place`
-      );
-      mapboxSearchResults.value = result.data.features;
-      console.log(mapboxSearchResults.value);
+        );
+        mapboxSearchResults.value = result.data.features;
+        console.log(mapboxSearchResults.value);
+      } catch {
+        searchError.value = true;
+      }
+
       return;
     };
     mapboxSearchResults.value = null;
